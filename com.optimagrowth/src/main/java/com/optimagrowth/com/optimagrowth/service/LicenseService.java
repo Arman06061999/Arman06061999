0c801587fe1f3af7a81ab74527201d22996bf467
Arman Antonyan <arman.treyder@bk.ru>
2024-01-15 15:05:53 +0300

    New Project Spring Congigure Cloud

---
+++ com.optimagrowth/src/main/java/com/optimagrowth/com/optimagrowth/service/LicenseService.java
@@ -0,0 +1,50 @@
+package com.optimagrowth.com.optimagrowth.service;
+
+import com.optimagrowth.com.optimagrowth.config.ServiceConfig;
+import com.optimagrowth.com.optimagrowth.model.License;
+import com.optimagrowth.com.optimagrowth.repository.LicenseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class LicenseService {
+    @Autowired
+    MessageSource messages;
+    @Autowired
+    private LicenseRepository licenseRepository;
+    @Autowired(required = false)
+    ServiceConfig config;
+    public License getLicense(String licenseId, String organizationId){
+        License license = licenseRepository
+                .findByOrganizationIdAndLicenseId(organizationId, licenseId);
+        if (null == license) {
+            throw new IllegalArgumentException(
+                    String.format(messages.getMessage(
+                                    "license.search.error.message", null, null),
+                            licenseId, organizationId));
+        }
+
+        return license.withComment(config.getProperty());
+    }
+    public License createLicense(License license){
+        license.setLicenseId(UUID.randomUUID().toString());
+        licenseRepository.save(license);
+        return license.withComment(config.getProperty());
+    }
+    public License updateLicense(License license){
+        licenseRepository.save(license);
+        return license.withComment(config.getProperty());
+    }
+    public String deleteLicense(String licenseId){
+        String responseMessage = null;
+        License license = new License();
+        license.setLicenseId(licenseId);
+        licenseRepository.delete(license);
+        responseMessage = String.format(messages.getMessage(
+                "license.delete.message", null, null),licenseId);
+        return responseMessage;
+    }
+}
